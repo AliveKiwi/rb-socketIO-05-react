a098dbf204d547a8be8ee440bd00d023845a2d17
AliveKiwi <prateekjais.dev@gmail.com>
2025-12-11 07:08:02 +0530

    2. Testing nodejs cluster module

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// Socket.io server that will service both node and react clients
+// Req
+// - socket.io
+// - @socket.io/cluster-adapter
+// - @socket.io/sticky
+
+// entrypoint for our cluster which will make workers and the workers will do the Socket.io handling
+// See https://github.com/elad/node-cluster-socket.io for more details
